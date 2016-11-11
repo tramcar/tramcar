c4232ebdfcf31933663191308617be9368b64ee1
wfhio <admin@wfh.io>
2016-11-10 21:36:52 -0500

    Add email to user registration form
    Closes #95

--- job_board/forms.py
+++ job_board/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 from job_board.models.company import Company
@@ -88,5 +89,13 @@ class CssUserCreationForm(UserCreationForm):
         super(CssUserCreationForm, self).__init__(*args, **kwargs)
         self.label_suffix = ''
         self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+    # UserCreationForm doesn't include email, so we add it and make it required
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    email = forms.EmailField(required=True)
