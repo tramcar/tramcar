a86763e193ce30d947be400520fe360641d11732
wfhio <admin@wfh.io>
2017-03-20 23:14:15 -0400

    Update model's __init__.py (#148)
    This commit updates models/__init__.py to import each model contained
    within.  This is necessary as when we tried to add a new model without
    it listed the makemigrations command kept failing saying no changes
    detected.
    
    Note that we add a # noqa comment to each line added above, otherwise
    flake8 will complain that there are unused imports.

---
+++ job_board/models/__init__.py
@@ -0,0 +1,5 @@
+from .category import Category       # noqa: F401
+from .company import Company         # noqa: F401
+from .country import Country         # noqa: F401
+from .job import Job                 # noqa: F401
+from .site_config import SiteConfig  # noqa: F401
