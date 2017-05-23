0e0565014c94885e08de9ecb69e108b5b68b9d8b
wfhio <admin@wfh.io>
2017-05-23 19:01:08 -0400

    Category RSS feeds
    This commit adds in an initial RSS feed for job categories.  At
    present, up to 30 max will be returned in the feed.  This may need to
    be made customisable at a later date.
    
    We also add font awesome fonts via CDN.  We may want to look at pulling
    these into source control in the near future.
    
    Closes #156

---
+++ job_board/views/feeds.py
@@ -0,0 +1,32 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.syndication.views import Feed
+from job_board.models.category import Category
+from job_board.models.job import Job
+
+
+class CategoryFeed(Feed):
+    def title(self, obj):
+        return '%s - %s Jobs Feed' % (obj.site.name, obj.name)
+
+    def link(self, obj):
+        return obj.get_absolute_url()
+
+    def description(self, obj):
+        return 'The latest %s jobs on %s' % (obj.name, obj.site.name)
+
+    def get_object(self, request, category_id, slug=None):
+        return Category.objects.get(
+                   pk=category_id, site_id=get_current_site(request).id
+               )
+
+    def items(self, obj):
+        return Job.objects.filter(category_id=obj) \
+                          .filter(paid_at__isnull=False) \
+                          .filter(expired_at__isnull=True) \
+                          .order_by('-paid_at')[:30]
+
+    def item_title(self, item):
+        return '%s @ %s' % (item.title, item.company.name)
+
+    def item_description(self, item):
+        return item.description
