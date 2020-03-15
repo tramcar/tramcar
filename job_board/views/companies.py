5409e7c8d53f7a2bee1b62411fcb2e879ba95de4
tramcarjobs <38306928+tramcarjobs@users.noreply.github.com>
2020-03-15 03:03:25 +0000

    Change button class

--- job_board/views/companies.py
+++ job_board/views/companies.py
@@ -50,7 +50,7 @@ def companies_new(request):
             company.save()
 
             if request.is_ajax():
-                # NOTE: CockroachDB's int is larger than JavaScript's number
+                # NOTE: CockroachDB's id is larger than JavaScript's number
                 #       precision (53 bits), which results in the id getting
                 #       rounded. Instead, we just convert the id to a string.
                 return JsonResponse(
